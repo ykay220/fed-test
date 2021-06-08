5ae574f51ec0ef1b96398782fc4cef955a71ce17
ykay220 <81273310+ykay220@users.noreply.github.com>
2021-06-08 17:45:38 -0400

    q4fedtest-yama-karimi

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,14 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}}'+
+                                '<span class="report_data--grey"> ({{file_size}} {{file_type}})</span>'+
+                                '</a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
@@ -24,6 +26,7 @@ var reportsWidget = {
     },
 
     renderReports: function(reports) {
+        console.log(reports);
         var inst = this,
             options = inst.options;
 
